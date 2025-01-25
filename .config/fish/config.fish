01664d46e1dec8cb4d791903323d478317718ac9
Tyler Lowrey <contact@tylerlowrey.com>
2025-01-24 20:43:13 -0500

    Changes for linux

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,13 +9,19 @@ set PATH $PATH $HOME/.cargo/bin
 set PATH $PATH $HOME/_dev/_bin
 set PATH $PATH $HOME/_dev/_bin/google-cloud-sdk/bin
 
+set -g fish_prompt_pwd_dir_length 20
+
 function fish_prompt
     set -l time (date +%H:%M:%S)
     printf '┌─[%s%s %s%s]-{%s%s%s}-[%s%s%s]\n└%s>%s ' \
         (set_color 8B93FF) (date +'%Y-%m-%d') $time (set_color normal) \
         (set_color F8F9D7) $USER (set_color normal) \
-        (set_color E49BFF) (prompt_pwd -D 20) (set_color normal) \
+        (set_color E49BFF) (prompt_pwd) (set_color normal) \
         (set_color EC8305) (set_color normal)
 end
 
-source /usr/local/opt/asdf/libexec/asdf.fish
+if test (uname -s) = Darwin
+    source /usr/local/opt/asdf/libexec/asdf.fish
+else
+    source ~/.asdf/asdf.fish
+end
--- install.fish
+++ install.fish
@@ -1,4 +1,4 @@
-#!/usr/local/bin/fish
+#!/usr/bin/env fish
 cd nvim-rs
 cargo build --release
 cd ..
