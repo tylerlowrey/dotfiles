4f0337c074f95c254d5a4cc2ec8402e8c5899189
Tyler Lowrey <contact@tylerlowrey.com>
2025-04-27 17:52:23 -0400

    Fix jdtls cmd location

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -1,5 +1,5 @@
 local config = {
-  cmd = { "~/.local/opt/jdtls/bin/jdtls" },
+  cmd = { vim.env.HOME .. "/.local/opt/jdtls/bin/jdtls" },
   root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
 }
 require("jdtls").start_or_attach(config)
